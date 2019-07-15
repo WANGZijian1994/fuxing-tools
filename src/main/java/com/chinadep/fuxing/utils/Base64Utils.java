15f151997e9bde1a059633310533e71cc7ae3224
jovimao <maojiawei2012180@126.com>
2019-07-15 11:10:34 +0800

    添加工具

---
+++ src/main/java/com/chinadep/fuxing/utils/Base64Utils.java
@@ -0,0 +1,61 @@
+package com.chinadep.fuxing.utils;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2019
+ * </p>
+ *
+ * @author Jovi
+ * @version 1.0
+ */
+@Slf4j
+public class Base64Utils {
+    /**
+     * BASE64解码工具
+     */
+    private static final Base64.Decoder decoder = Base64.getDecoder();
+
+    /**
+     * 批量解码base64
+     * @param list 需要解码的字符串
+     * @return 解码后的字符串
+     */
+    public static List<String> decode(List<String> list){
+        List<String> decodeList = Lists.newArrayList();
+        for(String line:list){
+            String decodeLine = decode(line);
+            decodeList.add(decodeLine);
+        }
+        return decodeList;
+    }
+
+    /**
+     * 单条数据解码
+     * @param str 需要解码的字符串
+     * @return 解码后的字符串
+     */
+    public static String decode(String str){
+        String decodeStr = "";
+        try {
+            decodeStr = new String(decoder.decode(str), StandardCharsets.UTF_8);
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        return decodeStr;
+    }
+
+}
---
+++ src/main/java/com/chinadep/fuxing/utils/FileUtils.java
@@ -0,0 +1,51 @@
+package com.chinadep.fuxing.utils;
+
+import com.github.marschall.lineparser.LineParser;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2019
+ * </p>
+ *
+ * @author Jovi
+ * @version 1.0
+ */
+@Slf4j
+public class FileUtils {
+
+    /**
+     * 读取文件
+     * @param filePath 文件
+     * @return
+     */
+    public static List<String> readFile(String filePath)  {
+        List<String> list = Lists.newArrayList();
+        LineParser parser = new LineParser();
+        Charset cs = StandardCharsets.UTF_8;
+        Path path = Paths.get(filePath);
+        try{
+            parser.forEach(path, cs, (line) -> {
+                list.add(line.getContent().toString());
+            });
+        }catch (IOException e){
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        return list;
+    }
+}
